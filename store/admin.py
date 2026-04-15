f42224fab22b0651570c935cc32c4670cfb6f736
SAJJADALIDON <sajjadalileads@gmail.com>
2026-04-15 17:39:35 +0500

    Rating and Final

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
-from .models import Product, Variation
+from .models import Product, ProductGallery, Variation, ReviewRating
+from django.utils.html import mark_safe
 
 
+# not working 
+# import thumbnails
+
+# @thumbnails.thumbnails('image')
+# class ProductGalleryInline(admin.TabularInline):
+#     model = ProductGallery
+#     extra = 1
+
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+    readonly_fields = ['image_preview']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="80" height="80" />')
+        return "No Image"
+
+    image_preview.short_description = 'Preview'
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
 
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value','is_active')
@@ -15,3 +36,5 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)    
 admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
